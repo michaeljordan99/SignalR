8843d8ffab1e3c0488f98a8533dd351ac5114b1c
Michael Jordan <michaeljordan99@hotmail.com>
2013-03-17 12:26:01 -0500

    initial commit

---
+++ SignalR/Example1/Example1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR.Example1
+{
+    [HubName("processTransaction")] 
+    public class ProcessTransactionHub : Hub
+    {
+        public void ProcessTransaction(string creditCardNumber, string expirationDate)
+        {
+            var transactionId = Guid.NewGuid(); 
+            //Clients.Others.update(transactionId.ToString(), creditCardNumber, expirationDate);
+            Clients.All.reportTransaction(transactionId.ToString(), creditCardNumber, expirationDate);
+
+        }
+    }
+}
---
+++ SignalR/MoveShape/MoveShape.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../scripts/jquery-1.8.3.js" />
+/// <reference path="../scripts/jquery.signalR-1.0.0.js" />
+/// <reference path="../Scripts/jquery-ui-1.9.2.js" />
+
+$(function() {
+
+    var hub = $.connection.moveShape,
+        $shape = $("#shape");
+
+    //This is what's listening to the Server ...
+    hub.client.shapeMoved = function (x, y) {
+        $shape.css({ left: x, top: y });
+    };
+
+    $.connection.hub.start().done(function () {
+        $shape.draggable({
+            drag: function () {
+
+                //This calls and updates the Server Method...
+                hub.server.moveShape(this.offsetLeft, this.offsetTop || 0);
+            }
+        });
+    });
+});
